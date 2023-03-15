f1875e97a754fad0498cc94c857850dd0e8f72dc
Andres Cespedes Morales <andrespedes@gmail.com>
2023-03-15 11:12:05 +0100

    Create performance test

---
+++ performance-test.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '10s',
+};
+
+export default function () {
+  http.get('https://orders-andresc-owggo4.5sc6y6-3.usa-e2.cloudhub.io/orders');
+  sleep(1);
+}
